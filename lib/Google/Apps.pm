62b69949482fe317b79e229fd71b762f3acc5a43
Robert Norris <rob@cataclysm.cx>
2009-10-27 08:02:59 +1100

    bootstrapped by Module::Starter

---
+++ lib/Google/Apps.pm
@@ -0,0 +1,110 @@
+package Google::Apps;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Google::Apps - The great new Google::Apps!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Google::Apps;
+
+    my $foo = Google::Apps->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Norris, C<< <rob at cataclysm.cx> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-google-apps at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Google-Apps>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Google::Apps
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Google-Apps>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Google-Apps>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Google-Apps>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Google-Apps/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Robert Norris.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Google::Apps
